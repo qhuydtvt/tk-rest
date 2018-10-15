f17f7f4d11ba687edd35cdfc35619ddfa352a376
minhduc9699 <30574677+minhduc9699@users.noreply.github.com>
2018-10-15 17:26:29 +0700

    update custom headers

--- tk_rest/tk_rest.py
+++ tk_rest/tk_rest.py
@@ -4,8 +4,11 @@ class TKEndPoint:
   def __init__(self, url, end_point):
     self.url = url + "/" + end_point
   
-  def get(self):
-    return requests.get(self.url)
+  def get(self, id=None, headers=None):
+    if id is None:
+      return requests.get(self.url, headers=headers)
+    else:
+      return requests.get(self.url + "/" + id, headers=headers)
   
   def post(self, data):
     return requests.post(self.url, json=data)
@@ -24,5 +27,5 @@ class TKRest:
     return TKEndPoint(self.url, attr)
 
 if __name__ == "__main__":
-  t = TKRest("https://tklms-api.herokuapp.com/api")
-  print(t.users.get().json())
+  t = TKRest("https://learn.techkids.vn/api")
+  print(t.classrooms.get().json())
